e081e461eb6c6d56bf541e094c06acc21fbafe39
15249293050 <15249293050@163.com>
2019-04-11 17:21:45 +0800

    优化AutoTableFieldCommentPlugin，修改peek为foreach

--- src/main/java/com/itfsw/mybatis/generator/plugins/AutoTableFieldCommentPlugin.java
+++ src/main/java/com/itfsw/mybatis/generator/plugins/AutoTableFieldCommentPlugin.java
@@ -10,23 +10,30 @@ import java.util.Map;
 
 public class AutoTableFieldCommentPlugin extends BasePlugin {
 
+	public static final String INTEGER = "integer";
+	public static final String LONG = "long";
+	public static final String FLOAT = "float";
+	public static final String DOUBLE = "double";
+	public static final String STRING = "string";
+
 	/*
 	 * Field添加注释(注释为表中的remarks字段)
 	 * 
 	 */
+
 	@Override
 	public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
 		try {
 			Map<String, IntrospectedColumn> columnMap = new HashMap<>();
 			introspectedTable.getAllColumns().forEach(column -> columnMap.put(column.getJavaProperty(), column));
-			topLevelClass.getFields().stream().peek(field -> {
+			topLevelClass.getFields().stream().forEach(field -> {
 				try {
 					IntrospectedColumn column = columnMap.get(field.getName());
 
 					StringBuilder sb = new StringBuilder();
 					sb.append(" * ");
 					sb.append(column.getRemarks());
-					
+
 					field.addJavaDocLine("/**");
 					field.addJavaDocLine(sb.toString().replace("\n", " "));
 					field.addJavaDocLine(" */");
@@ -34,6 +41,7 @@ public class AutoTableFieldCommentPlugin extends BasePlugin {
 				} catch (Exception ignored) {
 				}
 			});
+
 		} catch (Exception e) {
 			topLevelClass.addAnnotation(e.getMessage());
 		}
